fbad0ce8852e1aaa5d5ff24ac8efea37bc233026
Yazdani <yaz4dani@gmail.com>
2019-12-13 23:02:49 +0600

    Design

---
+++ lib/Home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
+
+
