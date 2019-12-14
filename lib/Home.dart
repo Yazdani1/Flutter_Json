606bcf495ce9d0a11504132133108ee7194a7802
Yazdani <yaz4dani@gmail.com>
2019-12-14 21:36:26 +0600

    Design

--- lib/Home.dart
+++ lib/Home.dart
@@ -4,14 +4,13 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => new _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
 
       appBar: AppBar(
-        title: Text("Json"),
+        title: Text("Jsjklon"),
         backgroundColor: Colors.green,
       ),
 
