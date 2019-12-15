bc37b35b23ae86a4b96b3048b6072ad014bfd689
Yazdani <yaz4dani@gmail.com>
2019-12-15 23:24:45 +0600

    Design

--- lib/Home.dart
+++ lib/Home.dart
@@ -15,12 +15,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   Future<List<User>>getallUser()async{
-    final response = await http.get("https://jsonplaceholder.typicode.com/users");
 
+    final response = await http.get("https://jsonplaceholder.typicode.com/users");
     List users = json.decode(response.body);
     return users.map((user)=>new User.fromJson(user)).toList();
-    
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +36,11 @@ class _HomeState extends State<Home> {
             future: getallUser(),
           builder: (context,AsyncSnapshot <List<User>> snapshot){
               if(snapshot.hasError || !snapshot.hasData){
+                
                 return Center(
                   child: CircularProgressIndicator(),
                 );
+
               }else{
                 return ListView.builder(
                     itemCount: snapshot.data.length,
@@ -48,9 +50,10 @@ class _HomeState extends State<Home> {
                       
                       return Card(
                         child: Container(
-                          height: 200.0,
+                          height: 500.0,
                           margin: EdgeInsets.all(10.0),
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
                               
                               Container(
@@ -73,7 +76,91 @@ class _HomeState extends State<Home> {
                                   ],
                                 ),
                               ),
-                              
+                              SizedBox(height: 7.0,),
+                              Divider(height: 30.0,indent: 50.0,color: Colors.black,),
+
+                              Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+
+                                    Text(user.username,
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                    ),
+                                    Text(user.email,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.phone,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.website,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.company.name,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.company.catchPhrase,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.company.bs,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.city,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.street,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.suite,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.city,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.zipcode,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.geo.lat,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    Text(user.address.geo.lng,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+
+
+                                  ],
+                                ),
+                              ),
+
+
                             ],
                           ),
                         ),
